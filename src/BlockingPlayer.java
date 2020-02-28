c0aa154a034349003c63e28a14d9edcdc237ebb7
talhakhalil0703 <tkhalil0703@gmail.com>
2020-02-27 19:03:28 -0700

    Blocking Player implemented

--- src/BlockingPlayer.java
+++ src/BlockingPlayer.java
@@ -9,4 +9,31 @@ public class BlockingPlayer extends RandomPlayer {
         super(name, mark);
     }
 
+    @Override
+    public void makeMove() {
+        for (int i =0; i <= 2; i++){
+            for (int j =0; j <= 2; j++){
+                if (testForBlocking(i,j)){
+                    this.getBoard().addMark(i, j, this.getMark());
+                    return;
+                }
+            }
+        }
+
+        super.makeMove();
+    }
+
+    private boolean testForBlocking(int i, int j) {
+        if (this.getBoard().getMark(i, j) == Constants.SPACE_CHAR) {
+            this.getBoard().addMark(i, j, this.getOpponent().getMark());
+            int winner = this.getBoard().checkWinner(this.getOpponent().getMark());
+            this.getBoard().addMark(i, j, Constants.SPACE_CHAR);
+            if (winner == 1)
+                return true;
+            else
+                return false;
+        }
+        return false;
+    }
+
 }
